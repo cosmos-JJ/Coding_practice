0fba0425ecef5a21f64a8c3a5c1473737208890f
Wooju Jeon <143316478+cosmos-JJ@users.noreply.github.com>
2023-08-30 23:33:37 +0900

    First, Implementing the Tower of Hanoi_2
    Becaus upload Failed, commit again

---
+++ Python/Tower of Hanoi.py
@@ -0,0 +1,25 @@
+# Tower of Hanoi
+# Rule1 : To be able to move one disk at a time
+# Rule2 : A large disk should not be on a small disk
+
+# When there is one disk
+def Hanoi_1(n):
+    return 1
+    
+Hanoi_1(1)
+    
+
+# When there are two disk
+def Hanoi_2(n):
+    count = 0
+    if n==1:
+        return 1
+    else:
+        count += 1
+        
+    
+Hanoi_2(2)
+
+
+   #Impossible to implement...... 
+   #I should think about it more..
